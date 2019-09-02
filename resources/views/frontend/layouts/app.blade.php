1f4d92b7b02af41e66074941d87b27b387e52c85
Lalitus <tallitosan@gmail.com>
2019-09-02 02:24:53 -0500

    fixed favicon

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
-    <link rel="icon" href="{{ asset('fi/favicon.ico') }}" type="image/gif" sizes="16x16">
+    <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/gif" sizes="16x16">
     <link rel="stylesheet" href="{{ asset('css/main.css') }}">
     <title>Welcome to my Portfolio</title>
     @yield('style')
