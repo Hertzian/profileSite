a925f3187594d3ef10bf1c0fe94813d861b0760b
Lalitus <tallitosan@gmail.com>
2019-09-02 20:32:21 -0500

    edit jobs and works datatables header

--- resources/views/backend/jobs.blade.php
+++ resources/views/backend/jobs.blade.php
@@ -7,8 +7,8 @@
 <!-- DataTables Example -->
 <div class="card mb-3">
     <div class="card-header">
-        <i class="fas fa-table"></i>
-        Data Table Example</div>
+        <i class="fas fa-fw fa-chart-area"></i>
+        Edit your Jobs</div>
     <div class="card-body">
         <div class="table-responsive">
         <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
