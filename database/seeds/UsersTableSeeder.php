be560bfcdbe37aa259668cb0365097f269187c01
Lalitus <tallitosan@gmail.com>
2020-06-07 04:23:33 -0500

    pull commit

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -19,7 +19,7 @@ class UsersTableSeeder extends Seeder
             'linkedin' => 'https://www.linkedin.com/in/eduardo-aguilar-1a68a5139/',
             'profesion' => '"Web Developer", "Programer", "Designer", "Entrepreneur"',
             'bio' => 'I am a fullstack web developer, I do CRM´s, web pages, custom systems. I want to become a web and a mobile development specialist. In my spare time I dedicate myself to model aircraft, assembly and flight drones in fpv.',
-            'phone' => '33 2707 0095',
+            'phone' => '00 0000 0000',
             'img' => 'LaloCV_1582163643.jpg',
             'background' => 'project1_1582166366.jpg',
             // 'active' => 1,
